f92a641863a275e4369c669c35e3d3300631ab64
ycylingithub <31947346+ycylingithub@users.noreply.github.com>
2018-02-14 18:12:47 +0800

    ES6 lesson14 Iterator接口

--- app/js/class/lesson14.js
+++ app/js/class/lesson14.js
@@ -1,1 +1,48 @@
+{
+    let arr=['hello','world'];
+    let map=arr[Symbol.iterator]();
+    console.log(map.next());
+    console.log(map.next());
+    console.log(map.next());
+}
 
+//自定义Iterator接口
+{
+    let obj={
+        start:[1,3,2],
+        end:[7,9,8],
+        [Symbol.iterator](){
+            let self=this;
+            let index=0;
+            let arr=self.start.concat(self.end);
+            let len=arr.length;
+
+            //返回一个对象，对象中应该包含next()方法
+            return {
+                next(){
+                    if(index<len){
+                        return {
+                            value:arr[index++],
+                            done:false
+                        }
+                    }else{
+                        return {
+                            value:arr[index++],
+                            done:true
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for(let key of obj){
+        console.log(key);
+    }
+}
+
+{
+    let arr=['hello','world'];
+    for(let value of arr){
+        console.log('value',value);
+    }
+}
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -9607,5 +9607,97 @@ module.exports = function (regExp, replace) {
 "use strict";
 
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+{
+    var arr = ['hello', 'world'];
+    var map = arr[Symbol.iterator]();
+    console.log(map.next());
+    console.log(map.next());
+    console.log(map.next());
+}
+
+//自定义Iterator接口
+{
+    var obj = _defineProperty({
+        start: [1, 3, 2],
+        end: [7, 9, 8]
+    }, Symbol.iterator, function () {
+        var self = this;
+        var index = 0;
+        var arr = self.start.concat(self.end);
+        var len = arr.length;
+
+        //返回一个对象，对象中应该包含next()方法
+        return {
+            next: function next() {
+                if (index < len) {
+                    return {
+                        value: arr[index++],
+                        done: false
+                    };
+                } else {
+                    return {
+                        value: arr[index++],
+                        done: true
+                    };
+                }
+            }
+        };
+    });
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = obj[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var key = _step.value;
+
+            console.log(key);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+}
+
+{
+    var _arr = ['hello', 'world'];
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+        for (var _iterator2 = _arr[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var value = _step2.value;
+
+            console.log('value', value);
+        }
+    } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                _iterator2.return();
+            }
+        } finally {
+            if (_didIteratorError2) {
+                throw _iteratorError2;
+            }
+        }
+    }
+}
+
 /***/ })
 /******/ ]);
