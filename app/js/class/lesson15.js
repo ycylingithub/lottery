3b9549711170157ffc7d16fa03ea11fe54f12d5b
ycylingithub <31947346+ycylingithub@users.noreply.github.com>
2018-02-14 21:31:48 +0800

    ES6 lesson15 Generator实例
    抽奖
    长轮询模拟

--- app/js/class/lesson15.js
+++ app/js/class/lesson15.js
@@ -58,3 +58,53 @@
 //   console.log(status.next());
 //   console.log(status.next());
 // }
+
+//前端对抽奖次数限制的做法
+{
+    let draw = function(count){
+        //具体抽奖逻辑
+        console.log(`剩余${count}次`);
+    }
+
+    let residue = function*(count){
+        while(count>0){
+            count--;
+            yield draw(count);
+        }
+    }
+
+    let star = residue(5);
+    let btn = document.createElement('button');
+    btn.id='start';
+    btn.textContent='抽奖';
+    document.body.appendChild(btn);
+    document.getElementById('start').addEventListener('click',function(){
+        star.next();
+    },false)
+}
+//长轮询的应用，实时取到服务端的数据
+{
+    let ajax = function *() {
+        yield new Promise(function (resolve,reject) {
+            setTimeout(function () {
+                resolve({code:0})
+            },200);
+        })
+    }
+
+    let pull =function () {
+        let generator = ajax();
+        let step = generator.next();
+        step.value.then(function (d) {
+            if(d.code!=0){
+                setTimeout(function () {
+                    console.info('wait');
+                    pull();
+                },1000);
+            }else{
+                consol.info(d);
+            }
+        })
+    }
+    pull();
+}
